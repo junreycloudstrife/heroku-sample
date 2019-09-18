18d2d33e897d7f9b2cbc44e5d5bab2b49c6856c5
junreycloudstrife <junrey.cloudstrife@gmail.com>
2019-09-18 08:11:51 +0800

    Changed from mqtt app to chat app

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-var http = require('http'),
-   fs = require('fs'),
-   // NEVER use a Sync function except at start-up!
-   index = fs.readFileSync(__dirname + '/index.html');
+// Express initializes app to be a function handler
+// that you can supply to an HTTP server.
+var app = require('express')();
+var http = require('http').Server(app);
 
-// Send index.html to all requests
-var app = http.createServer(function(req, res) {
-   res.writeHead(200, {'Content-Type': 'text/html'});
-   res.end(index);
+// Initialize a new instance of socket.io by passing
+// the http (the HTTP server) object.
+var io = require('socket.io')(http);
+
+// We define a route handler / that gets called when
+// we hit our website home.
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');    // This code servers the index.html
 });
 
-// app.listen(3000);
-app.listen(process.env.PORT);
+app.get('/index2', function(req, res){           // This is a trial and error part
+    res.sendFile(__dirname + '/index2.html');    // This code servers the index.html
+});
+ 
+io.on('connection', function(socket){           // Listen on the connection event when a user to access page
+    console.log('a user connected')                 // Logs on the terminal
+    io.emit('chat message', 'a user connected')     // Send to all client
+    socket.on('disconnect', function(){             // Listen on the disconnect event when a user disconnects
+        console.log("user disconnected")                // Logs on the terminal
+    })
+
+    socket.on('chat message', function(msg){    // Listens on the chat message event for incoming message
+        io.emit('chat message', msg);               // Sends to all client the message
+        console.log("chat message :" + msg)         // Logs the message to the terminal
+    });
+});
+
+
+// http.listen(3000, function(){                     // Make the http server listen on port 3000.
+//   console.log('listening on *:3000');
+// });
+
+http.listen(process.env.PORT, function(){                     // Make the http server listen on port set for environment.
+    console.log('listening on *:' + process.env.PORT);
+  });
